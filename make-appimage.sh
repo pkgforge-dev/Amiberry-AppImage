a02b965e1134776dc30c0be6fecc88faf077667e
Link4Electronics <link4electronics@gmail.com>
2026-08-15 19:48:56 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q amiberry | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q amiberry | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -14,8 +14,6 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/amiberry
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
