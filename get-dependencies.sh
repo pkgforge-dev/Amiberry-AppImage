f334f6413f725523cd15a7baec6cf2cb3644bbbf
Link4Electronics <link4electronics@gmail.com>
2026-08-31 20:09:37 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -45,5 +45,7 @@ mkdir -p ./AppDir/bin/data
 cd ./amiberry
 cmake -S ./ -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build -j$(nproc)
+make capsimg -j$(nproc)
+mv -v capsimg.so ../AppDir/bin
 mv -v build/amiberry roms whdboot ../AppDir/bin
 mv -v data/abr floppy_soundsdata ../AppDir/bin/data
