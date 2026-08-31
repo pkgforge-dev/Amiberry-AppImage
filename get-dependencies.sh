71b14ff72cbfdbfcd2a6b718f61dcca8d3fc4de8
Link4Electronics <link4electronics@gmail.com>
2026-08-31 19:46:47 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,14 +13,19 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package amiberry
+#make-aur-package amiberry
 
 # If the application needs to be manually built that has to be done down here
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+echo "Building stable version of Amiberry..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/BlitterStudio/amiberry"
+VERSION="$(curl -s https://api.github.com/repos/BlitterStudio/amiberry/releases/latest | grep '"tag_name"' | cut -d '"' -f 4)"
+git clone --recursive --depth 1 "$REPO" ./amiberry
+echo "$VERSION" > ~/version
+
+mkdir -p ./AppDir/bin
+cd ./amiberry
+cmake -S ./ -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build -j$(nproc)
